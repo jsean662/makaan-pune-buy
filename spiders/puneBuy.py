27c5e5cf2ea733b59c355e96e817ffc0ea8c7198
Jay Sawant <jsean662@gmail.com>
2016-11-16 13:19:28 +0530

    first commit

--- spiders/puneBuy.py
+++ spiders/puneBuy.py
@@ -18,7 +18,7 @@ class PuneSpider(scrapy.Spider):
     name = "puneBuy"
     allowed_domains = ["https://www.makaan.com/"]
 
-    start_urls = ['https://www.makaan.com/listings?sortBy=date-desc&listingType=buy&pageType=CITY_URLS&cityName=Pune&cityId=21&templateId=MAKAAN_CITY_LISTING_BUY&page=%s' % page for page in xrange(1,6)]
+    start_urls = ['https://www.makaan.com/listings?sortBy=date-desc&listingType=buy&pageType=CITY_URLS&cityName=Pune&cityId=21&templateId=MAKAAN_CITY_LISTING_BUY&page=%s' % page for page in xrange(1,891)]
   
     def parse(self, response):
         item = MakaanItem()
@@ -48,21 +48,21 @@ class PuneSpider(scrapy.Spider):
             item['listing_date'] = '0'  
             item['txn_type'] = 'None'  
             item['property_type'] = 'None'  
-            item['building_name'] = 'None'  
+            item['Building_name'] = 'None'  
             item['lat'] = '0'  
             item['longt'] = '0'  
             item['locality'] = 'None'  
-            item['status'] = 'None'  
+            item['Status'] = 'None'  
             item['listing_by'] = 'None'
             item['name_lister'] = 'None'  
-            item['selling_price'] = '0'  
-            item['monthly_rent'] = '0'  
+            item['Selling_price'] = '0'  
+            item['Monthly_Rent'] = '0'  
             item['details'] = 'None'  
             item['data_id'] = 'None'  
-            item['possession'] = '0'  
-            item['launch_date'] = '0'
+            item['Possession'] = '0'  
+            item['Launch_date'] = '0'
             item['price_per_sqft'] = '0'  
-            item['bua_sqft'] = '0'  
+            item['Bua_sqft'] = '0'  
             item['quality1'] = '0'
             item['quality2'] = '0'
             item['quality3'] = '0'
@@ -97,9 +97,9 @@ class PuneSpider(scrapy.Spider):
 
             item['city'] = data['cityName']
 
-            item['building_name'] = data['fullName']
-            if item['building_name'] == '':
-                item['building_name'] = 'None'
+            item['Building_name'] = data['fullName']
+            if item['Building_name'] == '':
+                item['Building_name'] = 'None'
                 qua4 -= 1
 
             item['config_type'] = data['bedrooms']+'BHK'
@@ -110,22 +110,22 @@ class PuneSpider(scrapy.Spider):
                 item['txn_type'] = 'Sale'
 
             if item['txn_type'] == 'Sale' or item['txn_type'] == 'Resale':
-                item['selling_price'] = data['price']
-                item['monthly_rent'] = '0'
+                item['Selling_price'] = data['price']
+                item['Monthly_Rent'] = '0'
 
             if 'Rental' in item['txn_type']:
-                item['monthly_rent'] = data['price']
-                item['selling_price'] = '0'
+                item['Monthly_Rent'] = data['price']
+                item['Selling_price'] = '0'
 
-            if item['selling_price'] == '0' and item['monthly_rent'] == '0':
+            if item['Selling_price'] == '0' and item['Monthly_Rent'] == '0':
                 item['price_on_req'] = 'TRUE'
                 qua4 -= 1
             else:
                 item['price_on_req'] = 'FALSE'
 
-            item['status'] = data['projectStatus']
-            if item['status'] == '':
-                item['status'] = 'None'
+            item['Status'] = data['projectStatus']
+            if item['Status'] == '':
+                item['Status'] = 'None'
            
             if (not data['verificationDate'] == ''):
                 dat = int(data['verificationDate'])/1000
@@ -144,16 +144,16 @@ class PuneSpider(scrapy.Spider):
 
             sqf = i.xpath('.//span[@class="size"]/text()').extract_first()
             try:
-                item['bua_sqft'] = re.findall('[0-9]+',sqf)
-                item['bua_sqft'] = ''.join(item['bua_sqft'])
+                item['Bua_sqft'] = re.findall('[0-9]+',sqf)
+                item['Bua_sqft'] = ''.join(item['Bua_sqft'])
             except:
-                item['bua_sqft'] = '0'
+                item['Bua_sqft'] = '0'
                 qua4 -= 1
 
-            if 'onstruction' in item['status']:
+            if 'onstruction' in item['Status']:
                 try:
                     date = i.xpath('div[@class="cardWrapper"]/div[@class="cardLayout clearfix"]/div[@class="infoWrap"]/div[@class="highlight-points"]/div[@class="dcol poss"]/div[1]/text()').extract_first()
-                    item['possession'] = dt.strftime(dt.strptime(date,'%b %Y'),'%m/%d/%Y %H:%M:%S')
+                    item['Possession'] = dt.strftime(dt.strptime(date,'%b %Y'),'%m/%d/%Y %H:%M:%S')
                     item['age'] = '0'
                 except:
                     print date
@@ -165,27 +165,26 @@ class PuneSpider(scrapy.Spider):
                 
             
 #Quality calculations
-            if(qua4 == 5):
+            if (((not item['Monthly_Rent'] == '0') and (not item['Bua_sqft']=='0') and (not item['Building_name']=='None') and (not item['lat']=='0')) or ((not item['Selling_price'] == '0') and (not item['Bua_sqft']=='0') and (not item['Building_name']=='None') and (not item['lat']=='0')) or ((not item['price_per_sqft'] == '0') and (not item['Bua_sqft']=='0') and (not item['Building_name']=='None') and (not item['lat']=='0'))):
                 item['quality4'] = 1
-            elif(qua4 < 4):
-                item['quality4'] = 0
-            else:
+            elif (((not item['price_per_sqft'] == '0') and (not item['Building_name']=='None') and (not item['lat']=='0')) or ((not item['Selling_price'] == '0') and (not item['Bua_sqft']=='0') and (not item['lat']=='0')) or ((not item['Monthly_Rent'] == '0') and (not item['Bua_sqft']=='0') and (not item['lat']=='0')) or ((not item['Selling_price'] == '0') and (not item['Bua_sqft']=='0') and (not item['Building_name']=='None')) or ((not item['Monthly_Rent'] == '0') and (not item['Bua_sqft']=='0') and (not item['Building_name']=='None'))):
                 item['quality4'] = 0.5
+            else:
+                item['quality4'] = 0
 
             if ((not item['mobile_lister'] == 'None') or (not item['listing_by'] == 'None') or (not item['name_lister'] == 'None')):
                 item['quality3'] = 1
             else:
                 item['quality3'] = 0              
 
-            if ((not item['launch_date'] == '0') or (not item['possession'] == '0')):
+            if ((not item['Launch_date'] == '0') or (not item['Possession'] == '0')):
                 item['quality2'] = 1
             else:
                 item['quality2'] = 0
 
-            if ((not item['building_name'] == 'None') and (not item['listing_date'] == '0') and (not item['txn_type'] == 'None') and (not item['property_type'] == 'None') and ((not item['selling_price'] == '0') or (not item['monthly_rent'] == '0'))):
+            if ((not item['Building_name'] == 'None') and (not item['listing_date'] == '0') and (not item['txn_type'] == 'None') and (not item['property_type'] == 'None') and ((not item['Selling_price'] == '0') or (not item['Monthly_Rent'] == '0'))):
                 item['quality1'] = 1
             else:
                 item['quality1'] = 0
 
-            qua4 = 5
             yield item
